bbdcb35b3be24e2e3cfd9e7e0c6dd1353793b1e7
Hamza-alikazmi <63kazmi.edu@gmail.com>
2025-10-12 14:41:45 +0500

    modified:   api/serve-file.js 	modified:   vercel.json

--- api/serve-file.js
+++ api/serve-file.js
@@ -1,10 +1,11 @@
-import fs from "fs";
-import path from "path";
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), req.query.file);
-  if (!fs.existsSync(filePath)) return res.status(404).send("Not found");
+  const file = req.query.file;
+  const url = `https://github.com/mrshah63/my-apt-repo/releases/download/v1.0/${file}`;
+  const response = await fetch(url);
+  if (!response.ok) return res.status(404).send('Not found');
   
-  res.setHeader("Content-Type", "application/octet-stream");
-  fs.createReadStream(filePath).pipe(res);
+  res.setHeader('Content-Type', 'application/x-debian-package');
+  response.body.pipe(res);
 }
